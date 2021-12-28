02c9337c33aa54627047fddfdbe702f5bb67797d
Dong-yeong0 <wjdehddud77@naver.com>
2021-12-28 21:51:47 +0900

    Vscode Github Connection Test

--- Answer/No_2564.java
+++ Answer/No_2564.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 
 public class No_2564 { // Main
 	public static void main(String[] args) throws IOException{
-		System.out.println("Connection Test");
+		System.out.println("Connection Test22");
 	}
 }
