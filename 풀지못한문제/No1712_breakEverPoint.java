2db9be039f2eb8102187720643c5eb2e71bd5f77
Dong-yeong0 <73709156+Dong-yeong0@users.noreply.github.com>
2022-01-08 23:07:06 +0900

    1712 손익분기점
    손익분기점이 무슨 뜻인지 몰라서 검색..
    1시간 삽질 끝에 성공

---
+++ 풀지못한문제/No1712_breakEverPoint.java
@@ -0,0 +1,27 @@
+package 풀지못한문제;
+import java.io.*;
+import java.util.*;
+/**
+ * No1712_breakEverPoint
+ * 2022-01-03
+ * @author Dong-yeong0
+ */
+
+public class No1712_breakEverPoint {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
+      int C = Integer.parseInt(st.nextToken());
+
+      if (C <= B) {
+        System.out.println("-1");
+      } 
+      else {
+        System.out.println((A/(C-B))+1);
+      }
+    }
+}
